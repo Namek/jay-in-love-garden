92246fda8238362415329e261fdfc72aa799499a
Kamil Dąbrowski <nameczanin@gmail.com>
2015-12-12 11:45:16 +0100

    Makes some changes for HTML/GWT build.

---
+++ desktop/src/net/namekdev/growing_love_garden/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package net.namekdev.growing_love_garden.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import net.namekdev.growing_love_garden.MyGardenLoveGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 768;
+		new LwjglApplication(new MyGardenLoveGame(), config);
+	}
+}
---
+++ html/src/net/namekdev/growing_love_garden/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package net.namekdev.growing_love_garden;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import net.namekdev.growing_love_garden.MyGardenLoveGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGardenLoveGame();
+        }
+}
